f8c4b420e9d415f59a78c646ce0cddd8160a01fd
itsmekatrine <itsmekatrine1@gmail.com>
2025-04-29 10:47:53 +0300

    Корректировка структуры проекта

--- src/main/java/ru/practicum/shareit/item/model/Item.java
+++ src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -1,8 +1,8 @@
 package ru.practicum.shareit.item.model;
 
 import lombok.Data;
-import ru.practicum.shareit.request.ItemRequest;
-import ru.practicum.shareit.user.User;
+import ru.practicum.shareit.request.model.ItemRequest;
+import ru.practicum.shareit.user.model.User;
 
 @Data
 public class Item {
