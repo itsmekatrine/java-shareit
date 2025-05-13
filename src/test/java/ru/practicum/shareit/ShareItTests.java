f3d9c8c9c7ebbb176b0596115380a3208a61b221
itsmekatrine <itsmekatrine1@gmail.com>
2025-05-13 16:26:03 +0300

    Внесение правок для ShareItTests.contextLoads

--- src/test/java/ru/practicum/shareit/ShareItTests.java
+++ src/test/java/ru/practicum/shareit/ShareItTests.java
@@ -1,9 +1,10 @@
 package ru.practicum.shareit;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE, classes = ShareItApp.class)
 class ShareItTests {
 
 	@Test
