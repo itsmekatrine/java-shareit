9db36512ec2cf277089d4931227dbee6b5dc851f
itsmekatrine <itsmekatrine1@gmail.com>
2025-05-14 10:31:09 +0300

    Добавление аннотации для классов-мапперов

--- src/main/java/ru/practicum/shareit/booking/mapper/BookingMapper.java
+++ src/main/java/ru/practicum/shareit/booking/mapper/BookingMapper.java
@@ -1,5 +1,7 @@
 package ru.practicum.shareit.booking.mapper;
 
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 import ru.practicum.shareit.booking.dto.BookingRequestDto;
 import ru.practicum.shareit.booking.dto.BookingDto;
 import ru.practicum.shareit.booking.model.Booking;
@@ -7,6 +9,7 @@ import ru.practicum.shareit.booking.model.BookingStatus;
 import ru.practicum.shareit.item.model.Item;
 import ru.practicum.shareit.user.model.User;
 
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class BookingMapper {
     // Booking → BookingDto
     public static BookingDto toDto(Booking booking) {
